781e7af946e18e23ab07ddb4756d9f50b3a53b3e
OlegBurov88 <ovburov88@gmail.com>
2024-09-12 22:40:55 +0500

    Лекция. Строки и индексация строк

---
+++ strings.py
@@ -0,0 +1,9 @@
+name = 'Vladimir'
+print('Hello, ' + name )
+print('Hello, ' * 2 + name * 3 )
+
+print(name[0], name[2], name[-1])
+print(name[0:4], name[:4])
+print(name[0:4:2], name[:4:2])
+print(name[3:])
+print(name[::-1])
