d2fe1ddf88872bf50f8652ba5887b51df5cf4123
OlegBurov88 <ovburov88@gmail.com>
2024-09-13 23:40:33 +0500

    Лекция. Переменные

---
+++ variable.py
@@ -0,0 +1,11 @@
+name = 'Oleg'
+print(name)
+
+name = 'Vladimir'
+print(name)
+
+date_of_birth = 'Февраль 1988'  #Snake case
+dateOfBirth = 'February 1988'   #Camel case
+
+print(date_of_birth)
+print(dateOfBirth)
