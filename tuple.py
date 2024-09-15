9be1ed0f71bfe4e50c74d8789b1bc9ce84a23c04
OlegBurov88 <ovburov88@gmail.com>
2024-09-15 12:52:49 +0500

    Лекция. Изменяемые и неизменяемые объекты. Кортежи

---
+++ tuple.py
@@ -0,0 +1,13 @@
+tuple_0 = [1, 2 ,3], 1, 2, 3
+tuple_1 = ([1, 2 ,3], 1, 2, 3)
+t_2 = tuple([[1, 2 ,3], 1, 2, 3])
+print(tuple_0)
+print(tuple_1)
+print(t_2, type(t_2))
+
+tuple_1 = tuple_1 + (4, 5) * 3
+print(tuple_1)
+print(t_2[0], t_2[2])
+tuple_0 [0][0] = tuple_0 [0][0] * 41
+tuple_0 [0][1] = tuple_0 [0][1] ** 10
+print(tuple_0)
