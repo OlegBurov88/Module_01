5f2a531a9731b66b8a0018bb9d249ed502941842
OlegBurov88 <ovburov88@gmail.com>
2024-09-21 08:20:28 +0500

    f - строка

--- module_1_2.py
+++ module_1_2.py
@@ -3,3 +3,4 @@ hours_spend = 1.5
 course_name = 'Python'
 time_one_task = hours_spend / complite_dz
 print('Курс:',course_name,', всего задач:',complite_dz,', затрачено часов:',hours_spend,', среднее время выполнения ',time_one_task,'часа.')
+print(f'Курс: {course_name}, всего задач: {complite_dz}, затрачено часов: {hours_spend}, среднее время выполнения {time_one_task} часа.')
